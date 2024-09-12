01f4eb0ccac6e8662df730bcd19558b1673b7558
matheussouzp <matheus_msp30@hotmail.com>
2024-09-12 16:22:41 -0300

    commit com functionalists

--- tasks/admin.py
+++ tasks/admin.py
@@ -3,6 +3,7 @@ from .models import Task
 
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('title', 'status', 'assigned_to', 'created_at')  # Exibe as colunas no admin
+
     list_filter = ('status',)  # Adiciona o filtro pelo campo status
 
 admin.site.register(Task)       # Registre o modelo Task
